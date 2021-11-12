6bf87ab6e329eb2656ed9bd0f3e35b827a9b8f13
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-12 16:29:20 -0700

    Added message to pompoptions

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function promptOptions() {
     {
         type: "list",
         name: "displayOptions",
+        message: "What would you like to do?",
         choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
     }
   ])
